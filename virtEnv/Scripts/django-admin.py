c09d7b2c3e9c7a6939fc73608b0a9cd4526390b9
drfrink <pradeepbhat92@gmail.com>
2017-05-29 23:46:42 -0400

    fixed gitignore

--- virtEnv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\whoami\documents\devprojects\adminlte_django\virtenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
