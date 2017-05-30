80f02e8354a0ee926999d4d0e7b5ecf8233ea6b2
drfrink <pradeepbhat92@gmail.com>
2017-05-29 23:42:00 -0400

    first push

---
+++ webappdemo/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+import views
+
+app_name='webappdemo'
+urlpatterns = [
+	url(r'^$', views.index, name='dashboard'),
+	url(r'^dashboard2/$', views.index2, name='dashboard2'),
+
+	url(r'^calendar/$', views.calendar, name='calendar'),
+	url(r'^widgets/$', views.widgets, name='widgets'),
+
+	##############
+	### Charts ###
+	##############
+	url(r'^chartjs/$', views.chartjs, name='chartjs'),
+	url(r'^morris/$', views.morrisCharts, name='morrisCharts'),
+	url(r'^flot/$', views.flotCharts, name='flotCharts'),
+	url(r'^inline/$', views.inlineCharts, name='inlineCharts'),
+	
+]
